9a758e251828f73ab3cf64dd73d77723b443b327
Michael Clemons <Mclemons@tql.com>
2024-06-12 16:11:25 -0400

    Add project files.

---
+++ forLoop/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace forLoop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            for (var i = 0; i <= 100; i++)
+            { 
+                Console.WriteLine(i);
+                if (i % 5 == 0 || i % 7 == 0)
+                {
+                    sum = sum + i;
+
+                }
+            }
+                Console.WriteLine(sum);
+        }
+    }
+}
