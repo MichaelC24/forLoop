51365f1977c18f9e06b3a2bc332372d32ec4a0b8
Michael Clemons <Mclemons@tql.com>
2024-06-13 10:56:52 -0400

    added in a fabinacci loop

--- forLoop/Program.cs
+++ forLoop/Program.cs
@@ -4,17 +4,35 @@
     {
         static void Main(string[] args)
         {
-            int sum = 0;
-            for (var i = 0; i <= 100; i++)
-            { 
-                Console.WriteLine(i);
-                if (i % 5 == 0 || i % 7 == 0)
-                {
-                    sum = sum + i;
+            //int sum = 0;
+            //for (var i = 0; i <= 100; i++)
+            //{ 
+            //    Console.WriteLine(i);
+            //    if (i % 5 == 0 || i % 7 == 0)
+            //    {
+            //        sum = sum + i;
 
-                }
+            //    }
+            //}
+            //    Console.WriteLine(sum);
+            
+            int firstnmbr = 0;
+            int secondnmbr = 1;
+            Console.Write($"{firstnmbr}, {secondnmbr} ");
+            int thirdnmbr = 0;
+
+            //for (int i = 0; i <= 10; i++)
+            while (thirdnmbr <= 25) 
+            {
+                thirdnmbr = firstnmbr + secondnmbr;
+                Console.Write($"{thirdnmbr}, ");
+                firstnmbr = secondnmbr;
+                secondnmbr = thirdnmbr;
+                
+                //if (firstnmbr > 25) { break;} 
+                
             }
-                Console.WriteLine(sum);
+            //Console.WriteLine(list[0]);
         }
     }
 }
